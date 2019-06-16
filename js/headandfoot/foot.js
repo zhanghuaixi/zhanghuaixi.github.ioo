c33fc4e6dc89387570db19ec4035a942467f020e
张淮汐 <49033099+zhanghuaixi@users.noreply.github.com>
2019-06-16 10:59:30 +0800

    Add files via upload

---
+++ js/headandfoot/foot.js
@@ -0,0 +1,20 @@
+document.writeln("<!Doctype html>");
+document.writeln("<html lang=\'zh\'>");
+document.writeln("  <head>");
+document.writeln("	<meta charset=\'UTF-8\'>");
+document.writeln("	<meta name=\'viewport\' content=\'width=device-width, initial-scale=1.0\'>");
+document.writeln("	<link rel=\'stylesheet\' type=\'text/css\' href=\'../css/1.css\'>");
+document.writeln("	<link href=\'//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\' rel=\'stylesheet\'>");
+document.writeln("  </head>");
+document.writeln("  <body>");
+
+document.writeln("<footer id=\'footer\'>");
+document.writeln("	  <p class=\'hhh\'>");
+document.writeln("	  </p>");
+document.writeln("	  <span class=\'hh\' id=\'runtm\'></span>");
+document.writeln("	</footer>");
+
+document.writeln("<script src=\'../js/1.js\'></script>");
+document.writeln("	<script>var menu = new Menu;</script>");
+document.writeln("  </body>");
+document.writeln("</html>");
